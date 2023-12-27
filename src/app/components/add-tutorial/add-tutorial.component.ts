8d69f584e3280394e7973e181bef8cfa7403155d
Deb <139592532+Deborah-McVey@users.noreply.github.com>
2023-12-27 11:43:41 -0500

    not compiling, no pagination yet

--- src/app/components/add-tutorial/add-tutorial.component.ts
+++ src/app/components/add-tutorial/add-tutorial.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/* import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-add-tutorial',
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrl: './add-tutorial.component.css'
 })
 export class AddTutorialComponent {
+
+} */
+
+import { Component } from '@angular/core';
+import { Tutorial } from '../../models/tutorial.model';
+import { TutorialService } from '../../services/tutorial.service';
+
+@Component({
+  selector: 'app-add-tutorial',
+  templateUrl: './add-tutorial.component.html',
+  styleUrls: ['./add-tutorial.component.css']
+})
+export class AddTutorialComponent {
+
+  tutorial: Tutorial = {
+    title: '',
+    description: '',
+    published: false
+  };
+  submitted = false;
+
+  constructor(private tutorialService: TutorialService) { }
+
+  saveTutorial(): void {
+    const data = {
+      title: this.tutorial.title,
+      description: this.tutorial.description
+    };
+
+    this.tutorialService.create(data)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.submitted = true;
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  newTutorial(): void {
+    this.submitted = false;
+    this.tutorial = {
+      title: '',
+      description: '',
+      published: false
+    };
+  }
 
 }
